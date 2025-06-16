92d322169108cfa31e8bdb384eb8f657aef4c0d7
nknuff <82107441+nknuff@users.noreply.github.com>
2025-06-16 11:35:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+navigator.geolocation.getCurrentPosition(position => {
+  const {latitude, longitude} = position.coords;
+  const map = L.map('map').setView([latitude, longitude], 15);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+  L.marker([latitude,longitude]).addTo(map).bindPopup("You are here").openPopup();
+  fetch(poi-data.json')
+    .then(response => resonse.json())
+    .then(data => {
+      data.forEach(poi => {
+        L.marker([poi.lat, poi.lon]).addTo(map)
+          .bindPopup(`<b>${poi.name}</b><br>${poi.description}`);
+      });
+    });
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
