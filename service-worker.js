425a905612091d3a9748dfdef3ccc6e3de2b8ec2
nknuff <82107441+nknuff@users.noreply.github.com>
2025-06-16 11:57:28 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('explore-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/poi-data.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+                      
+                      
+      
+        
