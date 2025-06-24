2d4cb0b3a82bdb138e27c445136e41bdebf1a945
nknuff <82107441+nknuff@users.noreply.github.com>
2025-06-24 18:37:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-navigator.geolocation.getCurrentPosition(position => {
-  const {latitude, longitude} = position.coords;
-  const map = L.map('map').setView([latitude, longitude], 15);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-  L.marker([latitude,longitude]).addTo(map).bindPopup("You are here").openPopup();
-  fetch('poi-data.json')
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(poi => {
-        L.marker([poi.lat, poi.lon]).addTo(map)
-          .bindPopup(`<b>${poi.name}</b><br>${poi.description}`);
-      });
+const map = L.map('map').setView([30.557598, -98.373195], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+let userMarker;
+
+navigator.geolocation.watchPosition(
+  position => {
+    const { latitude, longitude } = position.coords;
+
+    if (!userMarker) {
+      userMarker = L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("You're here!")
+        .openPopup();
+      map.setView([latitude, longitude], 15);
+    } else {
+      userMarker.setLatLng([latitude, longitude]);
+    }
+  },
+  error => {
+    console.error("Geolocation error:", error);
+  },
+  {
+    enableHighAccuracy: true,
+    maximumAge: 10000
+  }
+);
+
+fetch('poi-data.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(poi => {
+      L.marker([poi.lat, poi.lon]).addTo(map)
+        .bindPopup(`<b>${poi.name}</b><br>${poi.description}`);
     });
-});
+  });
+
